d27b2d501c87a4bb503fd33b922d2d334417fbe4
LD <liudaax@126.com>
2020-12-10 18:13:37 +0800

    Modules Init
    Signed-off-by: LD <liudaax@126.com>

---
+++ aradin-spring/aradin-spring-core/src/main/java/org/aradin/spring/core/App.java
@@ -0,0 +1,13 @@
+package org.aradin.spring.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
