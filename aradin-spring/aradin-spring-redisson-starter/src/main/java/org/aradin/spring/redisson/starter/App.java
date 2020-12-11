247152d92f413c58649358764df2d8d15e4ae65c
LD <liudaax@126.com>
2020-12-11 11:14:30 +0800

    The first version
    Signed-off-by: LD <liudaax@126.com>

---
+++ aradin-spring/aradin-spring-redisson-starter/src/main/java/org/aradin/spring/redisson/starter/App.java
@@ -0,0 +1,13 @@
+package org.aradin.spring.redisson.starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ aradin-spring/aradin-spring-swagger-starter/src/main/java/org/aradin/spring/swagger/starter/App.java
@@ -0,0 +1,13 @@
+package org.aradin.spring.swagger.starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
