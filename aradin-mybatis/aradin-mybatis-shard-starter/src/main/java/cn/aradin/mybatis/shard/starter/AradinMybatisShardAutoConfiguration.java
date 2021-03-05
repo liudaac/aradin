37026c1f915a65d9c708ee5686c7f03ee3ee11e8
LD <liudaax@126.com>
2021-03-05 18:05:28 +0800

    Add ShardingSphere Module
    Signed-off-by: LD <liudaax@126.com>

---
+++ aradin-mybatis/aradin-mybatis-shard-starter/src/main/java/cn/aradin/mybatis/shard/starter/AradinMybatisShardAutoConfiguration.java
@@ -0,0 +1,13 @@
+package cn.aradin.mybatis.shard.starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class AradinMybatisShardAutoConfiguration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
