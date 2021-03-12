e7b6cfe022437a3dbd0801cbd08bed72d1b62e7f
LD <liudaax@126.com>
2021-03-12 16:46:09 +0800

    Optimize
    Signed-off-by: LD <liudaax@126.com>

--- aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/net/http/error/HttpError.java
+++ aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/net/http/error/HttpError.java
@@ -3,10 +3,12 @@ package cn.aradin.spring.core.net.http.error;
 import java.io.Serializable;
 
 import cn.aradin.spring.core.net.http.code.CodedEnum;
+import lombok.Builder;
 import lombok.Data;
 
 @SuppressWarnings("serial")
 @Data
+@Builder
 public class HttpError implements Serializable {
 	private CodedEnum coded;
 	private Integer code;
@@ -18,6 +20,11 @@ public class HttpError implements Serializable {
 	}
 
 	public HttpError(CodedEnum coded, String msg) {
+		this.coded = coded;
+		this.msg = msg;
+	}
+	
+	public HttpError(CodedEnum coded, Integer code, String msg) {
 		this.coded = coded;
 		this.msg = msg;
 	}
