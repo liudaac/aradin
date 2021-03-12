091833115a0b22f1e855521fc15e647d945e9b3d
LD <liudaax@126.com>
2021-03-12 16:51:43 +0800

    Simple Nacos And Sentinel
    Signed-off-by: LD <liudaax@126.com>

--- aradin-alibaba/aradin-alibaba-nacos-starter/src/main/java/cn/aradin/alibaba/nacos/starter/AradinNacosAutoConfiguration.java
+++ aradin-alibaba/aradin-alibaba-nacos-starter/src/main/java/cn/aradin/alibaba/nacos/starter/AradinNacosAutoConfiguration.java
@@ -1,4 +1,7 @@
 package cn.aradin.alibaba.nacos.starter;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AradinNacosAutoConfiguration {
 }
--- aradin-alibaba/aradin-alibaba-sentinel-starter/src/main/java/cn/aradin/alibaba/sentinel/starter/AradinSentinelAutoConfiguration.java
+++ aradin-alibaba/aradin-alibaba-sentinel-starter/src/main/java/cn/aradin/alibaba/sentinel/starter/AradinSentinelAutoConfiguration.java
@@ -1,4 +1,7 @@
 package cn.aradin.alibaba.sentinel.starter;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AradinSentinelAutoConfiguration {
 }
