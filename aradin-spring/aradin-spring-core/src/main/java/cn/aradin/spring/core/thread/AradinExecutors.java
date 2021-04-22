c3af5fe90b94f90de284890001ef5294d61a1795
LD <liudaax@126.com>
2021-04-22 11:47:23 +0800

    Fix Executor Bug
    Signed-off-by: LD <liudaax@126.com>

--- aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/thread/AradinExecutors.java
+++ aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/thread/AradinExecutors.java
@@ -5,11 +5,13 @@ import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import cn.aradin.spring.core.thread.handler.AradinRejectedExecutionHandler;
+
 public class AradinExecutors {
 
 	public static Executor newFixedThreadPool(String group, int corePoolSize, int maximumPoolSize, int queueSize, long keepAliveTime) {
 		Executor executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MILLISECONDS,
-				new LinkedBlockingDeque<Runnable>(queueSize), new AradinThreadFactory(group), null);
+				new LinkedBlockingDeque<Runnable>(queueSize), new AradinThreadFactory(group), new AradinRejectedExecutionHandler(group));
 		return executor;
 	}
 }
