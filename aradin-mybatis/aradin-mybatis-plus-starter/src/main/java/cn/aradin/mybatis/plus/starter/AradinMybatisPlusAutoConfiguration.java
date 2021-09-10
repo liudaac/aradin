491e804aeac115fded270072014c3f641e11a04b
LD <liudaax@126.com>
2021-09-10 15:52:36 +0800

    Delay the upgrade for springboot 2.5.x
    Signed-off-by: LD <liudaax@126.com>

--- aradin-mybatis/aradin-mybatis-plus-starter/src/main/java/cn/aradin/mybatis/plus/starter/AradinMybatisPlusAutoConfiguration.java
+++ aradin-mybatis/aradin-mybatis-plus-starter/src/main/java/cn/aradin/mybatis/plus/starter/AradinMybatisPlusAutoConfiguration.java
@@ -1,10 +1,10 @@
 package cn.aradin.mybatis.plus.starter;
 
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 
 @Configuration
 @EnableTransactionManagement
@@ -14,10 +14,10 @@ public class AradinMybatisPlusAutoConfiguration {
 	 * Add Page Module To Support Page Select
 	 * @return
 	 */
-//	@Bean
-//    public PaginationInterceptor paginationInterceptor() {
-//		PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-//        paginationInterceptor.setLimit(-1l);
-//		return paginationInterceptor;
-//    }
+	@Bean
+    public PaginationInterceptor paginationInterceptor() {
+		PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setLimit(-1l);
+		return paginationInterceptor;
+    }
 }
