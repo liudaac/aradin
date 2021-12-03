0e4bdb1585b915942d762ac7e2574f908c452de3
LD <liudaax@126.com>
2021-12-03 11:27:50 +0800

    Merge From swagger
    Signed-off-by: LD <liudaax@126.com>

--- aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/net/http/body/Resp.java
+++ aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/net/http/body/Resp.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 import com.alibaba.fastjson.JSONObject;
 
 import cn.aradin.spring.core.net.http.code.AradinCodedEnum;
+import cn.aradin.spring.core.net.http.code.CodedEnum;
 import cn.aradin.spring.core.net.http.error.HttpError;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,47 +17,48 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Resp<T> implements Serializable {
-	private int code = 0;
+public class Resp<C extends CodedEnum, T> implements Serializable {
+	private C code;
 	private String msg;
 	private T data;
-
-	public Resp<T> data(T data) {
+	public Resp<C, T> data(T data) {
 		this.data = data;
 		return this;
 	}
-
-	public Resp<T> code(int code) {
+	public Resp<C, T> code(C code) {
 		this.code = code;
 		return this;
 	}
-
-	public Resp<T> msg(String msg) {
+	public Resp<C, T> msg(String msg) {
 		this.msg = msg;
 		return this;
 	}
-
+	
 	public boolean ifok() {
-		return code == AradinCodedEnum.OK.getCode();
+		return code.getCode()==0;
 	}
-
+	
 	public String toString() {
 		return JSONObject.toJSONString(this);
 	}
-
-	public static Resp<Object> ok() {
-		return new Resp<Object>();
+	public static Resp<AradinCodedEnum, Object> ok() {
+		return new Resp<AradinCodedEnum, Object>().code(AradinCodedEnum.OK);
 	}
-
-	public static <A> Resp<A> ok(A data) {
-		return new Resp<A>().data(data);
+	public static <A> Resp<AradinCodedEnum, A> ok(A data) {
+		return new Resp<AradinCodedEnum, A>().data(data).code(AradinCodedEnum.OK);
 	}
-
-	public static Resp<Object> error(HttpError error) {
+	public static <C extends CodedEnum> Resp<C, Object> ok(C coded) {
+		return new Resp<C, Object>(coded, null, null);
+	}
+	public static <C extends CodedEnum, A> Resp<C, A> ok(C coded, A data) {
+		return new Resp<C, A>(coded, null, data);
+	}
+	public static Resp<CodedEnum, Object> error(HttpError error) {
 		return error(error, null);
 	}
-
-	public static Resp<Object> error(HttpError error, String alias) {
-		return new Resp<Object>().code(error.getCode()!=null?error.getCode():error.getCoded().getCode()).msg(StringUtils.isNotBlank(alias) ? alias : error.getMsg());
+	public static Resp<CodedEnum, Object> error(HttpError error, String alias) {
+		return new Resp<CodedEnum, Object>()
+			.code(error.getCoded())
+			.msg(StringUtils.isNotBlank(alias)?alias:error.getMsg());
 	}
 }
