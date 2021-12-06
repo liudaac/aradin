f64679cb93d02a51339e867f76aa7b3fc7cbfe90
LD <liudaax@126.com>
2021-12-06 15:02:20 +0800

    Add cookie config
    Signed-off-by: LD <liudaax@126.com>

--- aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/AradinCoreAutoConfiguration.java
+++ aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/AradinCoreAutoConfiguration.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import cn.aradin.spring.core.bean.AradinBeanFactory;
+import cn.aradin.spring.core.session.AradinSessionConfiguration;
 
 /**
  * Configuration Door
  *
  */
 @Configuration
-@Import(AradinBeanFactory.class)
+@Import({AradinBeanFactory.class, AradinSessionConfiguration.class})
 public class AradinCoreAutoConfiguration {
 }
