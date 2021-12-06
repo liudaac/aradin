ded9c8975446444a5e6212755a81b52d74e0d91c
LD <liudaax@126.com>
2021-12-06 15:10:17 +0800

    Add cookie config
    Signed-off-by: LD <liudaax@126.com>

---
+++ aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/session/AradinSessionConfiguration.java
@@ -0,0 +1,42 @@
+package cn.aradin.spring.core.session;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class AradinSessionConfiguration {
+	
+	@Value("${spring.session.cookie.name:SESSION}")
+    private String cookieName;
+	
+	@Value("${spring.session.cookie.max-age:1800}")
+	private Integer maxAge;
+	
+	@Value("${spring.session.cookie.domain:}")
+	private String domain;
+	
+	/**
+	 * 参考 SpringHttpSessionConfiguration
+	 * @return
+	 */
+	@Bean
+	public CookieSerializer createDefaultCookieSerializer() {
+		DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+		if (StringUtils.isNotBlank(cookieName)) {
+			cookieSerializer.setCookieName(cookieName);
+			if (maxAge != null && maxAge > 0) {
+				cookieSerializer.setCookieMaxAge(maxAge);
+			}
+			if (StringUtils.isNotBlank(domain)) {
+				cookieSerializer.setDomainName(domain);
+			}
+		}
+		cookieSerializer.setRememberMeRequestAttribute(SpringSessionRememberMeServices.REMEMBER_ME_LOGIN_ATTR);
+		return cookieSerializer;
+	}
+}
