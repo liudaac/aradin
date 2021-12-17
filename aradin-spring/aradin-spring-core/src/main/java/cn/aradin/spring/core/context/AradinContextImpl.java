46e6615b5913bdfac459a3f643b7069c6b03104b
LD <liudaax@126.com>
2021-12-17 14:10:14 +0800

    Add Threadlocal Context
    Signed-off-by: LD <liudaax@126.com>

---
+++ aradin-spring/aradin-spring-core/src/main/java/cn/aradin/spring/core/context/AradinContextImpl.java
@@ -0,0 +1,33 @@
+package cn.aradin.spring.core.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AradinContextImpl implements AradinContext {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9093095285749631903L;
+	
+	private Map<String, Object> data = new HashMap<String, Object>();
+	
+	@Override
+	public Map<String, Object> getData() {
+		// TODO Auto-generated method stub
+		return data;
+	}
+
+	@Override
+	public void putData(String key, Object value) {
+		// TODO Auto-generated method stub
+		data.put(key, value);
+	}
+
+	@Override
+	public Object getData(String key) {
+		// TODO Auto-generated method stub
+		return data.get(key);
+	}
+
+}
