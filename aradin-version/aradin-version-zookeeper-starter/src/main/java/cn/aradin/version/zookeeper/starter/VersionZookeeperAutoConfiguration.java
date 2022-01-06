feff47dc1f931d94aadb1cfec792bac4b1181c2e
LD <liudaax@126.com>
2022-01-06 19:25:16 +0800

    Fix dependency circle
    Signed-off-by: LD <liudaax@126.com>

--- aradin-version/aradin-version-zookeeper-starter/src/main/java/cn/aradin/version/zookeeper/starter/VersionZookeeperAutoConfiguration.java
+++ aradin-version/aradin-version-zookeeper-starter/src/main/java/cn/aradin/version/zookeeper/starter/VersionZookeeperAutoConfiguration.java
@@ -15,7 +15,6 @@ import cn.aradin.version.zookeeper.starter.handler.VersionZookeeperBroadHandler;
 import cn.aradin.version.zookeeper.starter.handler.VersionsNodeHandler;
 import cn.aradin.zookeeper.boot.starter.ZookeeperBootAutoConfiguration;
 import cn.aradin.zookeeper.boot.starter.handler.INodeHandler;
-import cn.aradin.zookeeper.boot.starter.manager.ZookeeperClientManager;
 import cn.aradin.zookeeper.boot.starter.properties.ZookeeperProperties;
 
 /**
@@ -36,8 +35,7 @@ public class VersionZookeeperAutoConfiguration {
 	
 	@Bean
 	public IVersionBroadHandler versionBroadHandler(VersionProperties versionProperties,
-			IVersionGentor versionGentor,
-			ZookeeperClientManager zookeeperClientManager) {
-		return new VersionZookeeperBroadHandler(versionProperties, versionGentor, zookeeperClientManager);
+			IVersionGentor versionGentor) {
+		return new VersionZookeeperBroadHandler(versionProperties, versionGentor);
 	}
 }
