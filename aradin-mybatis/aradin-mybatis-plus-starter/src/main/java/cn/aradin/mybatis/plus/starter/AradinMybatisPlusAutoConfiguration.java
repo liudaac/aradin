40266b42fbd81d2d1849c7d86923e4b7cb48bf26
LD <liudaax@126.com>
2022-01-21 18:10:51 +0800

    Fix bug
    Signed-off-by: LD <liudaax@126.com>

--- aradin-mybatis/aradin-mybatis-plus-starter/src/main/java/cn/aradin/mybatis/plus/starter/AradinMybatisPlusAutoConfiguration.java
+++ aradin-mybatis/aradin-mybatis-plus-starter/src/main/java/cn/aradin/mybatis/plus/starter/AradinMybatisPlusAutoConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
