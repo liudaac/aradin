7f18d23c189840fa27264a52ed5aa3f2aa24f4f0
LD <liudaax@126.com>
2022-01-21 16:30:22 +0800

    Fix page search problem
    Signed-off-by: LD <liudaax@126.com>

--- aradin-mybatis/aradin-mybatis-plus-starter/src/main/java/cn/aradin/mybatis/plus/starter/AradinMybatisPlusAutoConfiguration.java
+++ aradin-mybatis/aradin-mybatis-plus-starter/src/main/java/cn/aradin/mybatis/plus/starter/AradinMybatisPlusAutoConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
 @Configuration
@@ -17,9 +19,9 @@ public class AradinMybatisPlusAutoConfiguration {
 	 */
 	@Bean
 	@ConditionalOnMissingBean
-    public PaginationInnerInterceptor paginationInterceptor() {
-		PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor();
-        paginationInterceptor.setMaxLimit(-1l);
-		return paginationInterceptor;
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
