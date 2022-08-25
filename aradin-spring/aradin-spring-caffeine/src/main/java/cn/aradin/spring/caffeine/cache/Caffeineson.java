3730d3f93d34ebfa7466f9723e437fe8de2c784b
daliu <daliu@BIH-D-7449.cn.net.ntes>
2022-08-25 10:03:04 +0800

    Fix caffeine cannot cache null problem Move security module to 0.0.4
    Signed-off-by: daliu <daliu@BIH-D-7449.cn.net.ntes>

--- aradin-spring/aradin-spring-caffeine/src/main/java/cn/aradin/spring/caffeine/cache/Caffeineson.java
+++ aradin-spring/aradin-spring-caffeine/src/main/java/cn/aradin/spring/caffeine/cache/Caffeineson.java
@@ -95,7 +95,7 @@ public class Caffeineson extends AbstractValueAdaptingCache {
 						return (T)value;
 					}
 					value = valueLoader.call();
-					put(key, value);
+					put(key, toStoreValue(value));
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					try {
@@ -115,7 +115,7 @@ public class Caffeineson extends AbstractValueAdaptingCache {
 	@Override
 	public void put(Object key, Object value) {
 		// TODO Auto-generated method stub
-		caffeineCache.put(key, value);
+		caffeineCache.put(key, toStoreValue(value));
 	}
 
 	@Override
@@ -126,7 +126,7 @@ public class Caffeineson extends AbstractValueAdaptingCache {
 			synchronized (key.toString().intern()) {
 				prevValue = caffeineCache.getIfPresent(key);
 				if (prevValue == null) {
-					caffeineCache.put(key, value);
+					caffeineCache.put(key, toStoreValue(value));
 					prevValue = value;
 				}
 			}
