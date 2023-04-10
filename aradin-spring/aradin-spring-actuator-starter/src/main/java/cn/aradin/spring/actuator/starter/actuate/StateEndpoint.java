eb5fdd4a26e0f893718ad2692736bc80d027e720
daliu <liudaax@126.com>
2023-04-10 14:43:48 +0800

    Fix endpoint problem
    Signed-off-by: daliu <liudaax@126.com>

--- aradin-spring/aradin-spring-actuator-starter/src/main/java/cn/aradin/spring/actuator/starter/actuate/StateEndpoint.java
+++ aradin-spring/aradin-spring-actuator-starter/src/main/java/cn/aradin/spring/actuator/starter/actuate/StateEndpoint.java
@@ -2,6 +2,8 @@ package cn.aradin.spring.actuator.starter.actuate;
 
 import java.time.Duration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.health.HealthComponent;
@@ -11,9 +13,11 @@ import org.springframework.boot.actuate.health.HealthEndpointGroups;
 
 import cn.aradin.spring.actuator.starter.context.DeployContext;
 
-@Endpoint(id = "state", enableByDefault = true)
+@Endpoint(id = "health", enableByDefault = true)
 public class StateEndpoint extends HealthEndpoint {
 
+	private final static Logger log = LoggerFactory.getLogger(StateEndpoint.class);
+	
 	public StateEndpoint(HealthContributorRegistry registry, HealthEndpointGroups groups,
 			Duration slowIndicatorLoggingThreshold) {
 		super(registry, groups, slowIndicatorLoggingThreshold);
@@ -21,9 +25,13 @@ public class StateEndpoint extends HealthEndpoint {
 	}
 	
 	@ReadOperation
-	public HealthComponent state() {
+	public HealthComponent health() {
 		if (DeployContext.isStopped()||DeployContext.isStopping()) {
+			log.error("Context is stopping or already stopped");
 			throw new RuntimeException("Context is stopping or already stopped");
+		}
+		if (log.isDebugEnabled()) {
+			log.debug("Context is checking");
 		}
 		HealthComponent health = super.health();
 		return health;
