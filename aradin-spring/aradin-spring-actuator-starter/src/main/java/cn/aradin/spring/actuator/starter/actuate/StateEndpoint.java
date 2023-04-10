b25c9403eb4501ff3e09d4c12708805dd98e39a7
daliu <liudaax@126.com>
2023-04-10 14:16:02 +0800

    Add state endpoint
    Signed-off-by: daliu <liudaax@126.com>

--- aradin-spring/aradin-spring-actuator-starter/src/main/java/cn/aradin/spring/actuator/starter/actuate/StateEndpoint.java
+++ aradin-spring/aradin-spring-actuator-starter/src/main/java/cn/aradin/spring/actuator/starter/actuate/StateEndpoint.java
@@ -11,17 +11,17 @@ import org.springframework.boot.actuate.health.HealthEndpointGroups;
 
 import cn.aradin.spring.actuator.starter.context.DeployContext;
 
-@Endpoint(id = "health", enableByDefault = true)
-public class HealthStateEndpoint extends HealthEndpoint {
+@Endpoint(id = "state", enableByDefault = true)
+public class StateEndpoint extends HealthEndpoint {
 
-	public HealthStateEndpoint(HealthContributorRegistry registry, HealthEndpointGroups groups,
+	public StateEndpoint(HealthContributorRegistry registry, HealthEndpointGroups groups,
 			Duration slowIndicatorLoggingThreshold) {
 		super(registry, groups, slowIndicatorLoggingThreshold);
 		// TODO Auto-generated constructor stub
 	}
 	
 	@ReadOperation
-	public HealthComponent health() {
+	public HealthComponent state() {
 		if (DeployContext.isStopped()||DeployContext.isStopping()) {
 			throw new RuntimeException("Context is stopping or already stopped");
 		}
--- aradin-spring/aradin-spring-actuator-starter/src/main/java/cn/aradin/spring/actuator/starter/configure/StateEndpointAutoConfiguration.java
+++ aradin-spring/aradin-spring-actuator-starter/src/main/java/cn/aradin/spring/actuator/starter/configure/StateEndpointAutoConfiguration.java
@@ -3,23 +3,22 @@ package cn.aradin.spring.actuator.starter.configure;
 import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnAvailableEndpoint;
 import org.springframework.boot.actuate.autoconfigure.health.HealthEndpointProperties;
 import org.springframework.boot.actuate.health.HealthContributorRegistry;
-import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.HealthEndpointGroups;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import cn.aradin.spring.actuator.starter.AradinActuatorAutoConfiguration;
-import cn.aradin.spring.actuator.starter.actuate.HealthStateEndpoint;
+import cn.aradin.spring.actuator.starter.actuate.StateEndpoint;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnAvailableEndpoint(endpoint = HealthStateEndpoint.class)
+@ConditionalOnAvailableEndpoint(endpoint = StateEndpoint.class)
 @AutoConfigureAfter(AradinActuatorAutoConfiguration.class)
-public class HealthStateEndpointAutoConfiguration {
+public class StateEndpointAutoConfiguration {
 	
 	@Bean
-	HealthEndpoint healthEndpoint(HealthContributorRegistry registry, HealthEndpointGroups groups,
+	StateEndpoint stateEndpoint(HealthContributorRegistry registry, HealthEndpointGroups groups,
 			HealthEndpointProperties properties) {
-		return new HealthStateEndpoint(registry, groups, properties.getLogging().getSlowIndicatorThreshold());
+		return new StateEndpoint(registry, groups, properties.getLogging().getSlowIndicatorThreshold());
 	}
 }
