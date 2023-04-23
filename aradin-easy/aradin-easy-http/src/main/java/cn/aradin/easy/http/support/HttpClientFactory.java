d2329b5071773ab161c4787270ea0f62c7fc1f91
dxsz <dxsz@10.236.176.129>
2023-04-23 19:27:46 +0800

    Upgrade httpclient implementation
    Signed-off-by: dxsz <dxsz@10.236.176.129>

--- aradin-easy/aradin-easy-http/src/main/java/cn/aradin/easy/http/support/HttpClientFactory.java
+++ aradin-easy/aradin-easy-http/src/main/java/cn/aradin/easy/http/support/HttpClientFactory.java
@@ -12,47 +12,47 @@ import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.DefaultHostnameVerifier;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
-/**
- * httpclient 4.3.x
- * @author Yi
- *
- */
-@SuppressWarnings("deprecation")
-public class HttpClientFactory{
+public class HttpClientFactory {
 
 	public static HttpClient createHttpClient() {
 		try {
-			SSLContext sslContext = SSLContexts.custom().useSSL().build();
-			SSLConnectionSocketFactory sf = new SSLConnectionSocketFactory(sslContext,SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, (certificate, authType) -> true).build();
+			SSLConnectionSocketFactory sf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
 			return HttpClientBuilder.create().setSSLSocketFactory(sf).build();
 		} catch (KeyManagementException e) {
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return null;
 	}
 
-	public static HttpClient createHttpClient(int maxTotal,int maxPerRoute) {
+	public static HttpClient createHttpClient(int maxTotal, int maxPerRoute) {
 		try {
-			SSLContext sslContext = SSLContexts.custom().useSSL().build();
-			SSLConnectionSocketFactory sf = new SSLConnectionSocketFactory(sslContext,SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, (certificate, authType) -> true).build();
+			SSLConnectionSocketFactory sf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
 			PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
 			poolingHttpClientConnectionManager.setMaxTotal(maxTotal);
 			poolingHttpClientConnectionManager.setDefaultMaxPerRoute(maxPerRoute);
 			poolingHttpClientConnectionManager.setValidateAfterInactivity(5000);
-			return HttpClientBuilder.create()
-									.setConnectionManager(poolingHttpClientConnectionManager)
-									.setSSLSocketFactory(sf)
-									.build();
+			return HttpClientBuilder.create().setConnectionManager(poolingHttpClientConnectionManager)
+					.setSSLSocketFactory(sf).build();
 		} catch (KeyManagementException e) {
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
@@ -60,6 +60,7 @@ public class HttpClientFactory{
 
 	/**
 	 * Key store 类型HttpClient
+	 * 
 	 * @param keystore
 	 * @param keyPassword
 	 * @param supportedProtocols
@@ -67,25 +68,26 @@ public class HttpClientFactory{
 	 * @param maxPerRoute
 	 * @return
 	 */
-	public static HttpClient createKeyMaterialHttpClient(KeyStore keystore,String keyPassword,String[] supportedProtocols,int maxTotal,int maxPerRoute) {
+	public static HttpClient createKeyMaterialHttpClient(KeyStore keystore, String keyPassword,
+			String[] supportedProtocols, int maxTotal, int maxPerRoute) {
 		try {
-			RegistryBuilder<ConnectionSocketFactory> registryBuilder = RegistryBuilder.<ConnectionSocketFactory>create();  
-			ConnectionSocketFactory plainSF = new PlainConnectionSocketFactory();  
-			registryBuilder.register("http", plainSF);  
-			
-			//指定信任密钥存储对象和连接套接字工厂  
+			RegistryBuilder<ConnectionSocketFactory> registryBuilder = RegistryBuilder
+					.<ConnectionSocketFactory>create();
+			ConnectionSocketFactory plainSF = new PlainConnectionSocketFactory();
+			registryBuilder.register("http", plainSF);
+
+			// 指定信任密钥存储对象和连接套接字工厂
 			SSLContext sslContext = SSLContexts.custom().loadKeyMaterial(keystore, keyPassword.toCharArray()).build();
-			SSLConnectionSocketFactory sf = new SSLConnectionSocketFactory(sslContext,supportedProtocols,
-	                null,SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+			SSLConnectionSocketFactory sf = new SSLConnectionSocketFactory(sslContext, supportedProtocols, null,
+					new DefaultHostnameVerifier());
 			registryBuilder.register("https", sf);
 			Registry<ConnectionSocketFactory> registry = registryBuilder.build();
-			PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager(registry);
+			PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager(
+					registry);
 			poolingHttpClientConnectionManager.setMaxTotal(maxTotal);
 			poolingHttpClientConnectionManager.setDefaultMaxPerRoute(maxPerRoute);
-			return HttpClientBuilder.create()
-									.setConnectionManager(poolingHttpClientConnectionManager)
-									.setSSLSocketFactory(sf)
-									.build();
+			return HttpClientBuilder.create().setConnectionManager(poolingHttpClientConnectionManager)
+					.setSSLSocketFactory(sf).build();
 		} catch (KeyManagementException e) {
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
@@ -97,6 +99,5 @@ public class HttpClientFactory{
 		}
 		return null;
 	}
-
 
 }
--- aradin-easy/aradin-easy-http/src/main/java/cn/aradin/easy/http/support/LocalHttpClient.java
+++ aradin-easy/aradin-easy-http/src/main/java/cn/aradin/easy/http/support/LocalHttpClient.java
@@ -24,30 +24,34 @@ public class LocalHttpClient {
 
 	private static final Logger logger = LoggerFactory.getLogger(LocalHttpClient.class);
 	private static final String caller = System.getProperty("aradin.http.caller");
-	protected static HttpClient httpClient = HttpClientFactory.createHttpClient(100,20);
-	
-	private static Map<String,HttpClient> httpClient_mchKeyStore = new HashMap<String, HttpClient>();
-	public static void init(int maxTotal,int maxPerRoute){
-		httpClient = HttpClientFactory.createHttpClient(maxTotal,maxPerRoute);
+	protected static HttpClient httpClient = HttpClientFactory.createHttpClient(100, 20);
+
+	private static Map<String, HttpClient> httpClient_mchKeyStore = new HashMap<String, HttpClient>();
+
+	public static void init(int maxTotal, int maxPerRoute) {
+		httpClient = HttpClientFactory.createHttpClient(maxTotal, maxPerRoute);
 	}
 
 	/**
-	 * 初始化   MCH HttpClient KeyStore
-	 * @param keyStoreName  keyStore 名称
+	 * 初始化 MCH HttpClient KeyStore
+	 * 
+	 * @param keyStoreName     keyStore 名称
 	 * @param keyStoreFilePath 私钥文件路径
 	 * @param mch_id
 	 * @param maxTotal
 	 * @param maxPerRoute
 	 */
-	public static void initMchKeyStore(String keyStoreName,String keyStoreFilePath,String mch_id,int maxTotal,int maxPerRoute){
+	public static void initMchKeyStore(String keyStoreName, String keyStoreFilePath, String mch_id, int maxTotal,
+			int maxPerRoute) {
 		try {
 			KeyStore keyStore = KeyStore.getInstance(keyStoreName);
 			InputStream instream = LocalHttpClient.class.getClassLoader().getResourceAsStream(keyStoreFilePath);
 //			 FileInputStream instream = new FileInputStream(new File(keyStoreFilePath));
-			 keyStore.load(instream,mch_id.toCharArray());
-			 instream.close();
-			 HttpClient httpClient = HttpClientFactory.createKeyMaterialHttpClient(keyStore, mch_id, new String[]{"TLSv1"}, maxTotal, maxPerRoute);
-			 httpClient_mchKeyStore.put(mch_id, httpClient);
+			keyStore.load(instream, mch_id.toCharArray());
+			instream.close();
+			HttpClient httpClient = HttpClientFactory.createKeyMaterialHttpClient(keyStore, mch_id,
+					new String[] { "TLSv1" }, maxTotal, maxPerRoute);
+			httpClient_mchKeyStore.put(mch_id, httpClient);
 		} catch (KeyStoreException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
@@ -61,8 +65,7 @@ public class LocalHttpClient {
 		}
 	}
 
-
-	public static HttpResponse execute(HttpUriRequest request){
+	public static HttpResponse execute(HttpUriRequest request) {
 		try {
 			return httpClient.execute(request);
 		} catch (ClientProtocolException e) {
@@ -73,10 +76,10 @@ public class LocalHttpClient {
 		return null;
 	}
 
-	public static <T> T execute(HttpUriRequest request,ResponseHandler<T> responseHandler) throws Exception{
+	public static <T> T execute(HttpUriRequest request, ResponseHandler<T> responseHandler) throws Exception {
 		try {
 			if (logger.isDebugEnabled()) {
-				logger.debug(request.getMethod()+" "+request.getURI());
+				logger.debug(request.getMethod() + " " + request.getURI());
 			}
 			if (StringUtils.isNotBlank(caller)) {
 				request.addHeader("EASY-CALLER", caller);
@@ -89,16 +92,17 @@ public class LocalHttpClient {
 
 	/**
 	 * 数据返回自动JSON对象解析
+	 * 
 	 * @param request
 	 * @param clazz
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T executeJsonResult(HttpUriRequest request,Type clazz) throws Exception{
+	public static <T> T executeJsonResult(HttpUriRequest request, Type clazz) throws Exception {
 		if (logger.isDebugEnabled()) {
-			logger.debug(request.getMethod()+" "+request.getURI());
+			logger.debug(request.getMethod() + " " + request.getURI());
 		}
-		return (T)execute(request,JsonResponseHandler.createResponseHandler(clazz));
+		return (T) execute(request, JsonResponseHandler.createResponseHandler(clazz));
 	}
 }
