e7429df643d98e23145366715e145361b956a116
daliu <liudaax@126.com>
2023-06-25 19:01:58 +0800

    Redis verison module init
    Signed-off-by: daliu <liudaax@126.com>

---
+++ aradin-version/aradin-version-redis-starter/src/main/java/cn/aradin/version/redis/starter/handler/VersionRedisBroadHandler.java
@@ -0,0 +1,19 @@
+package cn.aradin.version.redis.starter.handler;
+
+import cn.aradin.version.core.handler.IVersionBroadHandler;
+
+public class VersionRedisBroadHandler implements IVersionBroadHandler {
+
+	@Override
+	public void broadcast(String group, String key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void broadcast(String group, String key, String version) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
