71160c9a9694c45e6ff92b295b435d85ff23635f
daliu <liudaax@126.com>
2023-08-10 14:27:01 +0800

    Modify interf
    Signed-off-by: daliu <liudaax@126.com>

--- aradin-spring/aradin-spring-redis-starter/src/main/java/cn/aradin/spring/redis/starter/core/RedisBucketTemplate.java
+++ aradin-spring/aradin-spring-redis-starter/src/main/java/cn/aradin/spring/redis/starter/core/RedisBucketTemplate.java
@@ -4,17 +4,15 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
 
 public class RedisBucketTemplate<V> extends RedisTemplate<String, V> {
 	
-	public <HK, HV> HashOperations<String, HK, HV> opsForHash(int bucket) {
+	public <HK, HV> BucketHashOperations<HK, HV> opsForHash(int bucket) {
 		return new BucketHashOperations<HK, HV>(this, bucket);
 	}
 	
-	public SetOperations<String, V> opsForSet(int bucket) {
+	public BucketSetOperations<String, V> opsForSet(int bucket) {
 		return new BucketSetOperations<String, V>(this, bucket);
 	}
 	
