d0ae922abe6f80744ae31e8309960bff83aaa27a
daliu <liudaax@126.com>
2023-11-08 11:56:20 +0800

    Add prospects module to support thoughts in future application.
    Signed-off-by: daliu <liudaax@126.com>

---
+++ aradin-prospects/aradin-prospects-webp/src/main/java/cn/aradin/prospects/webp/AradinWebpConfiguration.java
@@ -0,0 +1,12 @@
+package cn.aradin.prospects.webp;
+
+/**
+ * {@link https://cmake.org/download/}
+ * {@link https://chromium.googlesource.com/webm/libwebp/+/refs/tags/v1.3.2}
+ * {@link https://github.com/nintha/webp-imageio-core/tree/master}
+ * {@link https://zhuanlan.zhihu.com/p/48530191}
+ * @author daliu
+ *
+ */
+public class AradinWebpConfiguration {
+}
