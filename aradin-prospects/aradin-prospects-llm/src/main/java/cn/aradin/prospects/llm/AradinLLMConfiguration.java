8cde2340b6bce0b2c86c542db6df781a8f32132b
daliu <liudaax@126.com>
2023-12-21 19:28:12 +0800

    Add LLM-client module
    Signed-off-by: daliu <liudaax@126.com>

---
+++ aradin-prospects/aradin-prospects-llm/src/main/java/cn/aradin/prospects/llm/AradinLLMConfiguration.java
@@ -0,0 +1,13 @@
+package cn.aradin.prospects.llm;
+
+/**
+ * Hello world!
+ *
+ */
+public class AradinLLMConfiguration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
